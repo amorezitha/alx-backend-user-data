34291894d9ccaaae8cbc5d7626490a6bc8935237
amorezitha <amorezitha@gmail.com>
2024-06-13 04:29:52 -0700

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,12 +11,12 @@ Base = declarative_base()
 
 
 class User(Base):
-    """
-    A User class mapping to to table users
+    """Represents user object/table
     """
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
